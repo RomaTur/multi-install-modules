afbc18ce59d51c0cc1d7ac740ac6ab3ff5ddbe4d
Roman <borka.grr@gmail.com>
2019-03-07 08:44:34 +0300

    installing in dirs

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+#!/usr/bin/env node
+
 require('babel-register')({
 	presets: [ 'es2015' ],
 });
+
+const program = require('commander');
 const run = require('./lib/main');
-run();
+
+program
+	.version('0.0.1')
+	.description('Contact management system')
+	.action(()=> {
+		run();
+	});
+
+program.parse(process.argv);
