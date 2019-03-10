2647f8369be51b33dce41e2510aea15dcf92bdd6
Roman <borka.grr@gmail.com>
2019-03-10 17:58:29 +0300

    asking for dirs and installing node_modules in

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
-
 require('babel-register')({
 	presets: [ 'es2015' ],
 });
 
 const program = require('commander');
 const run = require('./lib/main');
+const { version } = require('./package.json');
 
 program
-	.version('0.0.1')
+	.version(version)
 	.description('Contact management system')
 	.action(()=> {
 		run();
