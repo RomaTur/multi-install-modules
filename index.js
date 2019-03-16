b60defbe1837e65b6d07db28b41fd25fb675761c
Roman <borka.grr@gmail.com>
2019-03-16 16:48:09 +0300

    added babel render and polyfill

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
-#!/usr/bin/env node
-
-const program = require('commander');
-const run = require('./lib/main');
-const version = require('./package.json').version;
-
-program
-	.version(version)
-	.description('Util for installing node_modules in all child directory')
-	.action(()=> {
-		run();
-	});
-
-program.parse(process.argv);
+require('@babel/polyfill');
+require('./build');
---
+++ src/index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const run = require('./lib/main');
+const version = require('./package.json').version;
+
+program
+	.version(version)
+	.description('Util for installing node_modules in all child directory')
+	.action(()=> {
+		run();
+	});
+
+program.parse(process.argv);
